7cc93f46347d017bde65c7febcd8726f09082bda
Nomeos <mathieu.rabot01@gmail.com>
2022-12-05 13:53:05 +0100

    Remove the title from the app.js and extract it as a component

--- framer-motion-app/src/App.js
+++ framer-motion-app/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import useWindowDimensions from "./component/utils/WindowDimension";
+import {Title} from "./component/Title";
 
 function App() {
+    const line = "Hello World";
+    const {width, height} = useWindowDimensions();
+    const middleX = (width / 2) - (line.length * 10);
+    const middleY = (height / 2) - 10;
+
+
     return (
-        <div className="App">
-          
-        </div>
+        <Title windowWidth={middleX} windowHeight={middleY} line={line}/>
     );
 }
 
