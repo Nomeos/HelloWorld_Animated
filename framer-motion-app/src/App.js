07c5c09d94a70c181048ab1525174d9b1e0c0def
Nomeos <mathieu.rabot01@gmail.com>
2022-12-12 11:22:43 +0100

    Make the default file use the container component

--- framer-motion-app/src/App.js
+++ framer-motion-app/src/App.js
@@ -1,21 +1,11 @@
-
 import './App.css';
-import useWindowDimensions from "./component/utils/WindowDimension";
-import {Title} from "./component/Title";
-import {Navigation} from "./component/Navigation";
+import logo from './logo.svg';
+import {Container} from "./component/Container";
+
 
 function App() {
-    const line = "Hello World";
-    const {width, height} = useWindowDimensions();
-    const middleX = (width / 2) - (line.length * 10);
-    const middleY = (height / 2) - 10;
-
-
     return (
-        <>
-            <Title windowWidth={middleX} windowHeight={middleY} line={line}/>
-            <Navigation height={height}/>
-        </>
+        <Container/>
     );
 }
 
